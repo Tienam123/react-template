1ac52ea59b783749ddf06d9822291c5b28e1c14e
Vladyslav <dr.tienam123@gmail.com>
2023-10-03 06:41:53 +0300

    learning send props in the parrent components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+import './scss/styles.scss';
+import { Header } from './pages/Header/Header';
+import { Main } from './pages/Main/Main';
+import { Sidebar } from './pages/Sidebar/Sidebar';
+import { Footer } from './pages/Footer/Footer';
 
 ReactDOM.createRoot( document.getElementById( 'root' ) ).render(
-  <App />,
+  <div className="container">
+    <Header />
+    <Main />
+    <Sidebar />
+    <Footer />
+  </div>,
 );
---
+++ src/pages/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.scss'
+
+
+export const Footer = () => {
+  return (
+    <footer className="Footer">
+      <h3>React Documentation site</h3>
+      <p>Copyrating React</p>
+    </footer>
+  )
+}
