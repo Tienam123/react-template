4de23a5e02f79a39f3b0f25319e1519362d59e69
Vladyslav Honchar <123368743+Tienam123@users.noreply.github.com>
2023-10-23 06:20:27 +0300

    add project react-swiper

---
+++ project1/src/index.js
@@ -0,0 +1,8 @@
+// enable Imports
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+/* Codes Parts */
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
