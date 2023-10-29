2876b1b7ca3ebf2a289d38a9f23055000e01cedc
Vladyslav Honchar <123368743+Tienam123@users.noreply.github.com>
2023-10-29 23:22:02 +0200

    Train with the react Routes

---
+++ react-project-7/src/index.js
@@ -0,0 +1,11 @@
+/* Default imports */
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// Code....
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+
+);
+
