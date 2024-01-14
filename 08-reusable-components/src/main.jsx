f1dd538972d2a75984a81e53e18b7a990366f2f5
Vladyslav Honchar <dr.tienam123@gmail.com>
2024-01-14 14:54:59 +0200

    Added 08-reusable-component-project

---
+++ 08-reusable-components/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './scss/style.scss';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <></>
+  </React.StrictMode>
+);
